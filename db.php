a4e5bc7d82c2e2f11f7c3feaa250257e8fac723a
jimmy00414 <jimmy6060633@gmail.com>
2024-05-30 13:51:15 +0800

    SQL2

--- db.php
+++ db.php
@@ -12,21 +12,73 @@ function all($table, $where) // 定義函式 'all'，用於查詢指定條件的
     return $rows; // 返回查詢結果。
 }
 
-function find($id) // 定義函式 'find'，用於查詢指定 id 的記錄。
-{
-    global $pdo; // 使用全局變量 $pdo 來進行資料庫操作。
-    $sql = "SELECT * FROM `dept` WHERE `id`='{$id}'"; // 構建 SQL 查詢語句，從 'dept' 表中選擇指定 id 的記錄。
-    $row = $pdo->query($sql)->fetch(PDO::FETCH_ASSOC); // 執行查詢並以關聯數組形式獲取單一結果。
 
-    return $row; // 返回查詢結果。
+// 簡易一
+// function find($id)    // 定義函式 'find'，用於查詢指定 id 的記錄。
+// {
+//     global $pdo;     // 使用全局變量 $pdo 來進行資料庫操作。
+//     $sql = "SELECT * FROM `dept` WHERE `id`='{$id}'";    // 構建 SQL 查詢語句，從 'dept' 表中選擇指定 id 的記錄。
+//     $row = $pdo->query($sql)->fetch(PDO::FETCH_ASSOC);   // 執行查詢並以關聯數組形式獲取單一結果。
+
+//     return $row;   // 返回查詢結果。
+// }
+
+
+// 方法二
+// function find($table, $arg)
+// {
+//     global $pdo;           // 使用全局變量 $pdo 來進行資料庫操作。
+
+//     if (is_array($arg)) {              // 如果 $arg 是陣列，則進行複合條件查詢。
+//         foreach ($arg as $key => $value) {         // 遍歷 $arg 陣列，將每個鍵值對轉換為 SQL 條件。
+//             $tmp[] = "`$key`='{$value}'";        // 將鍵和值轉換為 `key`='value' 的形式，並存入 $tmp 陣列中。
+//         }
+
+//         $sql = "SELECT * FROM `{$table}` WHERE " . join(" && ", $tmp);        // 將 $tmp 陣列中的條件用 " && " 連接，生成完整的 SQL 查詢語句。
+//     } else { // 如果 $arg 不是陣列，則進行單條件查詢。
+//         $sql = "SELECT * FROM `{$table}` WHERE `id`='{$arg}'";       // 生成查詢語句，查詢 id 等於 $arg 的記錄。
+//     }
+
+//     //echo $sql; // 可以解除註解以輸出生成的 SQL 查詢語句，用於調試。
+
+//     $row = $pdo->query($sql)->fetch(PDO::FETCH_ASSOC);            // 執行查詢並以關聯數組形式獲取單一結果。
+
+//     return $row;         // 返回查詢結果。
+// }
+
+
+方法三
+
+function find($table, $arg)
+{
+    global $pdo; // 使用全局變量 $pdo 來進行資料庫操作
+
+    $sql = "SELECT * FROM `{$table}` WHERE ";       // 開始構建 SQL 查詢語句，選取指定表中的所有字段並開始 WHERE 子句
+
+    if (is_array($arg)) { // 如果 $arg 是陣列
+        foreach ($arg as $key => $value) {        // 遍歷 $arg 陣列中的每個鍵值對
+            $tmp[] = "`$key`='{$value}'";        // 將每個鍵值對轉換為 `key`='value' 的 SQL 條件，並存入 $tmp 陣列中
+        }
+
+        $sql .= join(" && ", $tmp);         // 將 $tmp 陣列中的條件用 " && " 連接，並附加到 SQL 語句後面
+    } else { // 如果 $arg 不是陣列
+        $sql .= " `id`='{$arg}'";           // 構建查詢語句，查詢 id 等於 $arg 的記錄，並附加到 SQL 語句後面
+    }
+
+    //echo $sql; // 可以解除註解以輸出生成的 SQL 查詢語句，用於調試
+
+    $row = $pdo->query($sql)->fetch(PDO::FETCH_ASSOC);    // 執行查詢並以關聯數組形式獲取單一結果
+
+    return $row;    // 返回查詢結果
 }
+
 
 /**
  * 在頁面上快速顯示陣列內容
  * direct dump
+
  * @param $array 輸入的參數需為陣列
  */
-
 function dd($array) // 定義函式 'dd'，用於格式化輸出陣列內容，通常用於調試。
 {
     echo "<pre>"; // 開啟 HTML 預格式化文本標籤。
--- index.php
+++ index.php
@@ -1,23 +1,27 @@
+<style>
+    * {
+        font-family: 'Courier New', Courier, monospace;
+    }
+</style>
+<h1>自訂函式</h1>
 <?php
-include_once "db.php"; // 引入 db.php 文件，如果已經引入則不再引入。通常這個文件包含資料庫連接和相關函式。
+include_once "libray.php";
+
+$a = ['A', 'B', 'C', 'D', 'E'];
+$b = [
+    '姓名' => '劉勤永',
+    '學號' => '103001',
+    '數學' => '90',
+    '國文' => 67,
+    '英文' => 56
+];
+
+//dd($a);
+//dd($b);
+
+stars();
+stars('菱形', 11);
+stars('矩形', 7);
+
+
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"> <!-- 設置視窗大小，確保在不同裝置上有良好的顯示效果 -->
-    <title>自訂函式</title>
-</head>
-
-<body>
-    <?php
-
-    dd(all('students', " WHERE `id`<5")); // 呼叫 `all` 函式查詢 `students` 表中 `id` 小於 5 的所有記錄，並用 `dd` 函式顯示結果。
-    dd(find(3)); // 呼叫 `find` 函式查詢 `id` 為 3 的記錄，並用 `dd` 函式顯示結果。
-
-    ?>
-</body>
-
-</html>
